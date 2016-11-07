d468d42a0443a63b6918e7d3584b43a341f2c436
Huy Sophanna <PJ-Sophanna@MacProPJ-Sophanna3.local>
2016-11-07 15:48:25 +0700

    removed development files

--- .tmp/providers/auth-data.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class AuthData {
-    fireAuth: any;
-    userProfile: any;
-    constructor();
-    loginUser(email: string, password: string): any;
-    logoutUser(): any;
-    signupUser(email: string, password: string): any;
-    resetPassword(email: string): any;
-}
