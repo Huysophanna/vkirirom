1f98bafda1175230194b920840fa8eefdc5022ea
Huy Sophanna <PJ-Sophanna@MacProPJ-Sophanna3.local>
2016-11-07 15:42:57 +0700

    removed platforms

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -42,10 +42,10 @@ export class Dashboard {
         var options = {
 
           replaceLineBreaks: false, // true to replace \n by a new line, false by default
-          // android: {
-          //     //  intent: 'INTENT'  // Opens Default sms app
-          //     intent: '' // Sends sms without opening default sms app
-          //   }
+          android: {
+              //  intent: 'INTENT'  // Opens Default sms app
+              intent: '' // Sends sms without opening default sms app
+            }
         }
         console.log("ready");
         SMS.send(number, message, options)
