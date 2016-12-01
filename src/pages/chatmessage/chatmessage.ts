767149bbbefd4e6a164a2760f331d5b4f07d36bd
Huy Sophanna <PJ-Sophanna@MacProPJ-Sophanna3.local>
2016-12-01 16:25:09 +0700

    Added profile picture in side menu

--- src/pages/chatmessage/chatmessage.ts
+++ src/pages/chatmessage/chatmessage.ts
@@ -19,43 +19,43 @@ export class Chatmessage {
   static get parameters() {
     return [NgZone];
   }
-  isUser: boolean;
-  // chatsLength: number;
-  // timeLength: number;
-  // time: any;
-  // timeObj: any;
-  chats: any;
-  chatinp: any;
-  pkt: any;
-  socket: any;
-  userName: any;
-  userPhoto: any;
-  messageTitle: any;
-  userStatus: any;
+  isUser: boolean; chatsLength: number; timeLength: number; time: any; timeObj: any; chats: any; chatinp: any;
+  pkt: any; socket: any; userName: any; userPhoto: any; messageTitle: any; userStatus: any;
+
   constructor(public navCtrl: NavController, public ngzone: NgZone, private platform: Platform, private alertCtrl: AlertController) {
+        NativeStorage.getItem('userDetails')
+          .then(
+            data => {
+              this.userName = data.displayName;
+            },
+            error => console.error(error)
+        );
+
         this.isUser = false;
         this.ngzone = new NgZone({enableLongStackTrace: false});
         this.chats = [];
         this.userStatus = [];
         this.chatinp ='';
         this.pkt = {
-            data: '',
+            message: '',
+            username: 'Phanna',
             room: 'room1'
         };
-        // this.time = [];
+        this.time = [];
         this.socket = io.connect('http://110.74.203.152:3000');
         // console.log(this.timeObj);
         
         console.log("run");
-        this.socket.on(this.pkt.room + 'message', (msg) => {
+        this.socket.on(this.pkt.room + 'message', (data) => {
           // this.test = new Date();
           console.log("runn1");
           this.ngzone.run(() => {
+            this.content.scrollToBottom();
             console.log("run1");
-            this.chats.push(msg);
-            // this.time.push(this.timeObj = new Date());
-            // this.timeLength = this.time.length;
-            // this.chatsLength = this.chats.length;
+            this.chats.push(data);
+            this.time.push(this.timeObj = new Date());
+            this.timeLength = this.time.length;
+            this.chatsLength = this.chats.length;
             // console.log(this.time);
             if (this.isUser == true) {
               console.log("This is me !!!!");
@@ -73,9 +73,9 @@ export class Chatmessage {
           console.log("run2");
             this.ngzone.run(() => {
                 this.chats.push(userenter + ' has joined');
-                // this.time.push(this.timeObj = new Date());
-                // this.timeLength = this.time.length;
-                // this.chatsLength = this.chats.length;
+                this.time.push(this.timeObj = new Date());
+                this.timeLength = this.time.length;
+                this.chatsLength = this.chats.length;
                 console.log("run2");
                 // console.log(this.time);
                 
@@ -85,70 +85,51 @@ export class Chatmessage {
           console.log("run3");
             this.ngzone.run(() => {
                 this.chats.push(userleave + ' has left');
-                // this.time.push(this.timeObj = new Date());
-                // this.timeLength = this.time.length;
-                // this.chatsLength = this.chats.length;
+                this.time.push(this.timeObj = new Date());
+                this.timeLength = this.time.length;
+                this.chatsLength = this.chats.length;
                 console.log("run3");
                 // console.log(this.time);
                 
             });
         });
-
-      NativeStorage.getItem('userDetails').then(
-        data => {
-          console.log("runstorage");
-          this.userName = data.displayName;
-          this.userPhoto = data.photoURL;
-        },
-        error => console.error(error)
-      );
-      console.log("runend");
       
   }
   
   send(msg) {
-      // if ((<string> Network.connection === 'none') || (<string> Network.connection === 'ethernet')) {
-      //   let alert = this.alertCtrl.create({
-      //       title: "Something went wrong",
-      //       subTitle: "There was a problem with network connection. Try again in another minutes ...",
-      //       buttons: ["OK"]
-      //   });
-      //   alert.present();
-      // } else {
+      if ((<string> Network.connection === 'none') || (<string> Network.connection === 'ethernet')) {
+        let alert = this.alertCtrl.create({
+            title: "Something went wrong",
+            subTitle: "There was a problem with network connection. Try again in another minutes ...",
+            buttons: ["OK"]
+        });
+        alert.present();
+      } else {
         if (msg != '') {
-            this.pkt.data = msg;
+            this.pkt.message = msg;
             this.socket.emit('message', this.pkt);
             console.log("sms send");
         }
+      }
       this.chatinp = '';
       this.isUser = true;
 
   }
-
-  // doInfinite(infiniteScroll) {
-  //   console.log('Begin async operation');
-
-  //   setTimeout(() => {
-
-  //     console.log('Async operation has ended');
-  //     infiniteScroll.complete();
-  //   }, 1000);
-  // }
     
   ionViewDidEnter(){
     console.log("enteruser");
-      this.pkt.data = this.userName;
+      this.pkt.username = this.userName;
       if(this.userName==null) {
-        this.pkt.data = "Anonymous";
+        this.pkt.username = "Anonymous";
       }
       this.socket.emit('userentered', this.pkt);
       console.log("enteruser");
   }
                          
   ionViewDidLeave(){
-      this.pkt.data = this.userName;
+      this.pkt.username = this.userName;
       if(this.userName==null) {
-        this.pkt.data = "Anonymous";
+        this.pkt.username = "Anonymous";
       }
       this.socket.emit('userleave', this.pkt);
       console.log("userleave");
